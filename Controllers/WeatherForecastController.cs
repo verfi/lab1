8988b68c811516a3cf22e436d85564b9710659da
Damian Osetek <w65558@student.wsiz.edu.pl>
2022-10-20 12:55:10 +0200

    just a commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using lab1.Folder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get(int value)
+        {
+            var calculateInstance = new Calculate(value);
+            calculateInstance.Increase(3);
+            return calculateInstance.GetValue();
+        }
+    }
+}
---
+++ Folder/Calculate.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace lab1.Folder
+{
+    public class Calculate
+    {
+        public Calculate(int value)
+        {
+            Value = value;
+        }
+        private int Value { get; set; }
+
+        public void Increase(int value)
+        {
+            Value += value;
+        }
+        public void Decrease(int value)
+        {
+            Value -= value;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+    }
+}
