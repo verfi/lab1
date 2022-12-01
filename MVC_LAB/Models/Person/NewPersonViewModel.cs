86812fc82e7579bdaf5f07e611129c40cb65e819
Damian Osetek <w65558@student.wsiz.edu.pl>
2022-12-01 12:59:30 +0100

    Database

---
+++ MVC_LAB/Models/Person/NewPersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_LAB.Models.Person
+{
+    public class NewPersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+            
+        public GenderEnum Gender { get; set; }
+        public string City { get; set; }
+    }
+}
