bf7dbfbf5b22a46ad46a2d8f4fcda2901af63256
Damian Osetek <w65558@student.wsiz.edu.pl>
2022-12-08 13:12:13 +0100

    Fabulous Commit

---
+++ MVC_LAB/Models/Person/CreatePersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVC_LAB.Models.Person
+{
+    public class CreatePersonViewModel
+    {
+        public string Name { get; set; }
+        public List<SelectListItem> Gender { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MVC_LAB/Services/PersonService.cs
@@ -0,0 +1,50 @@
+﻿using MVC_LAB.Context;
+using MVC_LAB.Models.Person;
+
+namespace MVC_LAB.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly PersonContext _context;
+        public PersonService(PersonContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePerson(string name, string city, GenderEnum gender)
+        {
+            var lastId = _context.Persons.OrderByDescending(x => x.ID).FirstOrDefault()?.ID;
+            if(lastId != null)
+            {
+                _context.Persons.Add(new PersonModel()
+                {
+                    ID = (int)lastId + 1,
+                    Name = name,
+                    City = city,
+                    Gender = gender
+                });
+                _context.SaveChanges();
+            }
+        }
+
+        public void EditPerson(long id, string name, string city, GenderEnum gender)
+        {
+            var person = _context.Persons.FirstOrDefault(x => x.ID == id);
+            if(person != null)
+            {
+                person.Name = name;
+                person.City = city;
+                person.Gender = gender;
+                _context.Persons.Update(person);
+                _context.SaveChanges();
+            }
+        }
+        public PersonModel GetPersons(int id)
+        {
+            var person = _context.Persons.FirstOrDefault(x => x.ID == id);
+            return person ?? new PersonModel();
+        }
+
+    }
+
+}
